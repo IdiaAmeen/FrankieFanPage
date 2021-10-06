770f0a654a88a17e2849d5c381a7f901748243a7
Idiat Ameen <idi@Idiats-Air.lan>
2021-10-06 17:14:53 -0400

    iphone view

--- frankiepage/src/App.js
+++ frankiepage/src/App.js
@@ -26,18 +26,20 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <Route exact path="/">
-            <div class="balloon"></div>
-            <div class="balloon"></div>
-            <div class="balloon"></div>
-            <div class="balloon"></div>
-            <div class="balloon"></div>
-            <div class="balloon"></div>
-            <div class="balloon"></div>
+            <div className="balloons">
+              <div class="balloon"></div>
+              <div class="balloon"></div>
+              <div class="balloon"></div>
+              <div class="balloon"></div>
+              <div class="balloon"></div>
+              <div class="balloon"></div>
+              <div class="balloon"></div>
+            </div>
 
-            {/* <img
-            src="https://media.giphy.com/media/xDk2ZOgr8Tc0qexRWH/giphy.gif?cid=790b7611a331969841f6f55b5d49a51f77e96fcf28177c69&rid=giphy.gif&ct=g"
-            id="avi"
-          /> */}
+            <img
+              src="https://media.giphy.com/media/xDk2ZOgr8Tc0qexRWH/giphy.gif?cid=790b7611a331969841f6f55b5d49a51f77e96fcf28177c69&rid=giphy.gif&ct=g"
+              id="avi"
+            />
             <div className="gifs-box">
               <img src="https://i.imgflip.com/5p9825.gif" class="gif" />
               <img src="https://i.imgflip.com/5p97cl.gif" class="gif" />
@@ -46,6 +48,7 @@ class App extends React.Component {
               <img src="https://i.imgflip.com/5pdo1r.gif" class="gif" />
               <img src="https://i.imgflip.com/5p9857.gif" class="gif" />
             </div>
+
             <p id="greeting">Happy Birthday Frankie!</p>
             <div className="drop-down">
               {this.state.isDisplay && <Gift handleClose={this.handleClose} />}
@@ -53,6 +56,7 @@ class App extends React.Component {
             {/* <Link to="/video" id="enter">
               Enter
             </Link> */}
+
             <button className="nav-services-icons" onClick={this.handleClick}>
               Open
             </button>
